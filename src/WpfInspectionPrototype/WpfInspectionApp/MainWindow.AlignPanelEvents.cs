bd2888883de5412e1bb4aa9923082b1444e0a938
gyGwak <geonyeong.gwak@pemtron.com>
2026-05-12 20:12:39 +0900

    곽건영 / 기능 개선
    AlignPanel 이벤트 디스패치 4개 메서드를 partial 파일로 분리
    - MainWindow.AlignPanelEvents.cs 신규 partial 파일에 다음 메서드 이동:
      HandleAlignPanelUpdate, ApplyAlignPanelUpdate, AlignPanel_UpdateRequested, AlignPanel_ActionRequested
    - MainWindow.xaml.cs 본체에서 약 95줄 제거
    - 동일 partial 클래스라 동작은 동일

---
+++ src/WpfInspectionPrototype/WpfInspectionApp/MainWindow.AlignPanelEvents.cs
@@ -0,0 +1,103 @@
+using WpfInspectionApp.ViewModels;
+
+namespace WpfInspectionApp;
+
+public partial class MainWindow
+{
+    private void HandleAlignPanelUpdate(AlignPanelUpdateEffect update)
+    {
+        HandleUiChange(() => ApplyAlignPanelUpdate(update));
+    }
+
+    private void ApplyAlignPanelUpdate(AlignPanelUpdateEffect update)
+    {
+        if (update.HasFlag(AlignPanelUpdateEffect.PartTeachingUi))
+        {
+            UpdatePartTeachingUi();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.Model))
+        {
+            UpdateModelFromUi();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.AlgorithmPanels))
+        {
+            _viewModel.UpdateAlgorithmPanels();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.RoiDrawButton))
+        {
+            _viewModel.UpdateRoiDrawButtonState();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.ActiveRoiUi))
+        {
+            UpdateActiveRoiUi();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.MaskDensity))
+        {
+            UpdateMaskDensity();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.RoiText))
+        {
+            UpdateRoiText();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.RoiOverlays))
+        {
+            DrawRoiOverlays();
+        }
+
+        if (update.HasFlag(AlignPanelUpdateEffect.Threshold))
+        {
+            ScheduleThreshold();
+        }
+    }
+
+    private void AlignPanel_UpdateRequested(object? sender, AlignPanelUpdateRequestedEventArgs e)
+    {
+        switch (e.Kind)
+        {
+            case AlignPanelUpdateKind.SearchNum:
+                SearchNumCombo_SelectionChanged();
+                break;
+            case AlignPanelUpdateKind.SearchSize:
+                SearchSizeBox_TextChanged(e.Source);
+                break;
+            default:
+                HandleAlignPanelUpdate(e.Kind.ToEffect());
+                break;
+        }
+    }
+
+    private void AlignPanel_ActionRequested(object? sender, AlignPanelActionRequestedEventArgs e)
+    {
+        switch (e.Kind)
+        {
+            case AlignPanelActionKind.ActiveRoi:
+                _viewModel.SelectNextAlignRoi();
+                break;
+            case AlignPanelActionKind.DrawWindowRoi:
+                _viewModel.EnableWindowRoiDrawing();
+                break;
+            case AlignPanelActionKind.DrawAlgorithmRoi:
+                _viewModel.EnableAlgorithmRoiDrawing();
+                break;
+            case AlignPanelActionKind.Teach:
+                TeachActiveRoiSize();
+                break;
+            case AlignPanelActionKind.PartTeachingIc:
+                RunAlignPartTeaching(useGerber: false);
+                break;
+            case AlignPanelActionKind.PartTeachingOk:
+                RunAlignPartTeaching(useGerber: true);
+                break;
+            case AlignPanelActionKind.PartTeachingClose:
+                CloseAlignPartTeaching();
+                break;
+        }
+    }
+}
