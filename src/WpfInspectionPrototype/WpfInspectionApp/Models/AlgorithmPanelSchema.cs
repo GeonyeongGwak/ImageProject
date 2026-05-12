002ff5b8b1adea28180ebbea66b8d9e644f65db2
gyGwak <geonyeong.gwak@pemtron.com>
2026-05-12 11:21:04 +0900

    곽건영 / 기능 개선
    AlgorithmPanelSchema를 InspectionModel.cs에서 분리
    - Models/AlgorithmPanelSchema.cs 신규 파일로 이동
    - InspectionModel.cs는 도메인 데이터(AlgorithmPanelData, AlgorithmPanelTabData)만 유지

---
+++ src/WpfInspectionPrototype/WpfInspectionApp/Models/AlgorithmPanelSchema.cs
@@ -0,0 +1,54 @@
+namespace WpfInspectionApp.Models;
+
+public static class AlgorithmPanelSchema
+{
+    public static AlgorithmPanelData Create(InspectionAlgorithmData algorithm)
+    {
+        var catalog = AlgorithmCatalog.Find(algorithm.Type);
+        return catalog.Type switch
+        {
+            "AlgoAlign" => CreateDedicated(catalog, "Dedicated", ["Search", "BW Condition", "Inspection", "IPC"]),
+            "AlgoAlignEdge" => CreateDedicated(catalog, "Dedicated", ["Edge", "Range", "Measure", "Anchor", "Serialize"]),
+            _ => CreateDynamic(catalog)
+        };
+    }
+
+    private static AlgorithmPanelData CreateDynamic(AlgorithmCatalogItem catalog)
+    {
+        var family = catalog.ParameterFamily;
+        return new AlgorithmPanelData
+        {
+            AlgorithmType = catalog.Type,
+            PanelKind = "Dynamic",
+            ActiveTab = "Common",
+            Tabs =
+            [
+                Tab("Common", "Common", ["Common.bAlgoEnable", "Common.IsRequired", "Common.bAlgoGroup", "Common.LightTypeNum"]),
+                Tab("Condition", "Condition", [$"{family}.Use2D", $"{family}.Use3D", $"{family}.Threshold", $"{family}.Tolerance"]),
+                Tab("ROI", "ROI / Mask", ["ROI.UseAlgorithmRoi", "Mask.UseMask", "Mask.UsePolygon", "Mask.ExceptRoiCount"]),
+                Tab("Advanced", "Advanced", ["Common.ManualDefectCode", "Common.ManualSubDefectCode", "Command.ApplyAllTarget", "Command.TeachRequested"])
+            ]
+        };
+    }
+
+    private static AlgorithmPanelData CreateDedicated(AlgorithmCatalogItem catalog, string kind, string[] tabs)
+    {
+        return new AlgorithmPanelData
+        {
+            AlgorithmType = catalog.Type,
+            PanelKind = kind,
+            ActiveTab = tabs.FirstOrDefault() ?? "Common",
+            Tabs = tabs.Select(tab => Tab(tab, tab, [])).ToList()
+        };
+    }
+
+    private static AlgorithmPanelTabData Tab(string id, string title, IEnumerable<string> keys)
+    {
+        return new AlgorithmPanelTabData
+        {
+            Id = id,
+            Title = title,
+            ParameterKeys = keys.ToList()
+        };
+    }
+}
